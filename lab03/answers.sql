2ff9857398b2bdeb8beeb5be2feff8759982f0dc
Celina Zhao <celinazhao@dhcp-10-105-101-185.wireless.northwestern.private>
2022-04-11 16:14:11 -0500

    lab3

---
+++ lab03/answers.sql
@@ -0,0 +1,63 @@
+-- Exercise 1 (done for you):
+SELECT * FROM users;
+
+-- Exercise 2 (done for you):
+SELECT id, first_name, last_name 
+FROM users;
+
+-- Exercise 3
+SELECT id, first_name, last_name 
+FROM users
+ORDER BY last_name;
+
+-- Exercise 4
+SELECT id, image_url, user_id 
+FROM posts
+WHERE user_id=26;
+
+-- Exercise 5
+SELECT id, image_url, user_id 
+FROM posts
+WHERE user_id=26 OR user_id=12;
+
+-- Exercise 6
+SELECT count(*)
+FROM posts;
+
+-- Exercise 7
+SELECT user_id, count(user_id)
+FROM comments
+GROUP BY user_id
+ORDER BY count(user_id) DESC;
+
+-- Exercise 8
+SELECT posts.id, posts.image_url, posts.user_id, users.username, users.first_name, users.last_name 
+FROM posts 
+INNER JOIN users 
+ON posts.user_id = users.id 
+WHERE posts.user_id=26 or posts.user_id=12;
+
+-- Exercise 9
+SELECT posts.pub_date, following.following_id 
+FROM posts 
+INNER JOIN following 
+ON posts.user_id = following.following_id 
+WHERE following.user_id = 26
+ORDER BY following.following_id;
+
+-- Exercise 10
+
+-- Exercise 11
+INSERT INTO bookmarks(user_id, post_id)
+VALUES (26, 219), (26, 220), (26, 221);
+
+-- Exercise 12
+DELETE FROM bookmarks
+WHERE user_id=26 AND post_id IN (219, 220, 221);
+
+-- Exercise 13
+UPDATE users
+SET email='knick2022@gmail.com'
+WHERE id=26;
+
+-- Exercise 14
